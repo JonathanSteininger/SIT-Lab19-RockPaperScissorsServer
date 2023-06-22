c7343344eaa9d60cd21ad76e6fbc99c68f0b8367
Jonathan Steininger <jonathan.steininger@icloud.com>
2023-06-22 14:03:02 +1200

    Added a simple ClientForm to test ServerFunctionailty

---
+++ ClientForm/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RockPaperScissorNetworkLibrary;
+
+namespace ClientForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private NetworkConnection _conn;
+        private Player _player;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _conn = new NetworkConnection("127.0.0.1", 2046);
+            _player = new Player("Jonathan", "Jono123");
+            _conn.Connect();
+            _conn.Send(new GameSendData(DataSentType.PlayerInfo, _player));
+        }
+    }
+}
